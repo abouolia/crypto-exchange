07eaca6b2542f7f6628c63399a59c03b2cefe429
a.bouhuolia <a.bouhuolia@live.com>
2022-01-28 20:52:32 +0200

    feat(DataTable): add pagination to data table component.

--- src/components/datatable.tsx
+++ src/components/datatable.tsx
@@ -1,8 +1,9 @@
 import React from "react"
 import styled from "styled-components"
-import { useTable } from "react-table"
+import { Column, useTable, usePagination, useAsyncDebounce } from "react-table"
 import { Align } from "../common"
 
+const DataTableRoot = styled.div``
 const TableRoot = styled.table`
   width: 100%;
   padding: 0px;
@@ -62,47 +63,108 @@ const TH = styled.th<THInterface>`
   `}
 `
 
-export function DataTable({ columns, data }) {
+const Pagination = styled.div``
+
+interface DataTableProps {
+  columns: Column[]
+  data: Object[]
+  pagination?: boolean
+  manualPagination?: boolean
+  controlledPagesCount?: number
+  onFetchData?: (context: { pageIndex: number; pageSize: number }) => void
+}
+
+export function DataTable({
+  columns,
+  data,
+  pagination,
+  manualPagination,
+  onFetchData,
+  controlledPagesCount,
+}: DataTableProps) {
   // Use the state and functions returned from useTable to build your UI
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({
+  const instance = useTable(
+    {
       columns,
       data,
-    })
+      manualPagination,
+      pageCount: controlledPagesCount,
+    },
+    usePagination
+  )
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+
+    state: { pageIndex, pageSize },
+    previousPage,
+    nextPage,
+    canPreviousPage,
+    canNextPage,
+  } = instance
+
+  // Debounce our onFetchData call for 100ms
+  const onFetchDataDebounced = useAsyncDebounce(onFetchData, 100)
+
+  // When the these table states changes, fetch new data!
+  React.useEffect(() => {
+    // Every change will call our debounced function
+    onFetchDataDebounced({ pageIndex, pageSize })
+    // Only the last call after the 100ms debounce is over will be fired!
+  }, [onFetchDataDebounced, pageIndex, pageSize])
+
   // Render the UI for your table
   return (
-    <TableRoot {...getTableProps()}>
-      <Thead>
-        {headerGroups.map(headerGroup => (
-          <TR {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <TH {...column.getHeaderProps()} textAlign={column.textAlign}>
-                {column.render("Header")}
-              </TH>
-            ))}
-          </TR>
-        ))}
-      </Thead>
-
-      <Tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row)
-          return (
-            <TR {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return (
-                  <TD
-                    textAlign={cell.column.textAlign}
-                    {...cell.getCellProps()}
-                  >
-                    {cell.render("Cell")}
-                  </TD>
-                )
-              })}
+    <DataTableRoot>
+      <TableRoot {...getTableProps()}>
+        <Thead>
+          {headerGroups.map(headerGroup => (
+            <TR {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <TH {...column.getHeaderProps()} textAlign={column.textAlign}>
+                  {column.render("Header")}
+                </TH>
+              ))}
             </TR>
-          )
-        })}
-      </Tbody>
-    </TableRoot>
+          ))}
+        </Thead>
+
+        <Tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row)
+            return (
+              <TR {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (
+                    <TD
+                      textAlign={cell.column.textAlign}
+                      {...cell.getCellProps()}
+                    >
+                      {cell.render("Cell")}
+                    </TD>
+                  )
+                })}
+              </TR>
+            )
+          })}
+        </Tbody>
+      </TableRoot>
+
+      {pagination && (
+        <Pagination>
+          <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+            + Previous Page +{" "}
+          </button>
+
+          <button onClick={() => nextPage()} disabled={!canNextPage}>
+            + Next Page +{" "}
+          </button>
+        </Pagination>
+      )}
+    </DataTableRoot>
   )
 }

c04a30d5dd1c8fb8e9ae1e4f96534cf1f7be56d0
a.bouhuolia <a.bouhuolia@live.com>
2022-01-28 18:19:31 +0200

    feat: add application top bar.

---
+++ src/components/header.tsx
@@ -0,0 +1,47 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+import styled from "styled-components"
+
+const HeaderRoot = styled.header`
+  margin-bottom: 1.45rem;
+  background: rgb(22, 82, 240);
+  height: 65px;
+`
+
+const HeaderContainer = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1rem 1.0875rem;
+`
+
+const HeaderSiteTitle = styled.h1`
+  font-size: 25px;
+  color: #fff;
+  margin: 0;
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`
+
+interface HeaderProps {
+  siteTitle: string
+}
+
+const Header = ({ siteTitle }: HeaderProps) => (
+  <HeaderRoot>
+    <HeaderContainer>
+      <HeaderSiteTitle>
+        <Link to="/">{siteTitle}</Link>
+      </HeaderSiteTitle>
+    </HeaderContainer>
+  </HeaderRoot>
+)
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
